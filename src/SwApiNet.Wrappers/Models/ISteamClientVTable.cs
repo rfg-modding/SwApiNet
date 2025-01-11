fa1b4a15aa6b4b29e78da67fcc9236943e01a3bc
Rostislav Kirillov <rast@tochka.com>
2025-01-12 02:00:48 +0400

    feat: xmldoc example

--- src/SwApiNet.Wrappers/Models/ISteamClientVTable.cs
+++ src/SwApiNet.Wrappers/Models/ISteamClientVTable.cs
@@ -13,9 +13,20 @@ generated from this interface:
     * place for user overridable logic, see SteamClientVTable.InterceptWrapper
  * p/invoke static class wired to whatever needs be imported/exported
 */
+/// <summary>
+/// xmldocs are also wired for generated stuff
+/// </summary>
+/// <example>
+/// copy of generated code: <see cref="SteamClientVTableGeneratedExample"/><br/>
+/// place for user overrides: <see cref="SteamClientVTable.InterceptWrapper"/>
+/// </example>
 [VTableProxy]
 public unsafe interface ISteamClientVTable
 {
+    /// <summary>
+    /// test docs
+    /// </summary>
+    /// <remarks>more notes</remarks>
     public nint UnusedCreateSteamPipe { get; }
     public nint UnusedBReleaseSteamPipe { get; }
     public nint UnusedConnectToGlobalUser { get; }
@@ -31,6 +42,14 @@ public unsafe interface ISteamClientVTable
     public nint UnusedGetISteamGenericInterface { get; }
     public nint GetISteamUserStats { get; }
     public nint UnusedGetISteamGameServerStats { get; }
+    /// <summary>
+    /// blah blah
+    /// </summary>
+    /// <param name="thisPtr"></param>
+    /// <param name="user">always 1?</param>
+    /// <param name="pipe">always 1?</param>
+    /// <param name="versionStr">c-style string</param>
+    /// <returns></returns>
     nint GetISteamApps(SteamClient* thisPtr, HSteamUser user, HSteamPipe pipe, byte* versionStr);
     public nint GetISteamNetworking { get; }
     public nint GetISteamRemoteStorage { get; }
